8a840f8c91607c7ab3b7f8559b4c0bbef878f6f7
nourgaser <me@nourgaser.com>
2025-12-27 18:09:33 +0000

    delete useless file

--- run_librarian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-  -v $(pwd):/app \
-  -w /app \
-  -e CGO_ENABLED=0 \
-  golang:alpine \
-  sh -c "apk add git && go get gopkg.in/yaml.v3 && go get github.com/fsnotify/fsnotify && go build -o librarian librarian.go"
